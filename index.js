83cffc07215891ef14e121b88390afb1c48d6d0a
ilyeshr2 <ilyesharrache0@gmail.com>
2023-10-20 11:23:36 +0100

    The "LinkSaver-Extension" is a browser extension that allows users to save and manage input data and tabs with ease.
    Introducing the "LinkSaver-Extension" – your new tool for efficiently saving and organizing input data and tabs right from your browser.
    Features:
    Save text input using the "SAVE INPUT" button.
    Save the currently open tab with the "SAVE TAB" button.
    Manage your saved data and tabs from a simple and user-friendly interface.
    Conveniently delete all saved items with the "DELETE ALL" button.

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
-let input_btn =document.getElementById("inp_btn")
-let tab =document.getElementById("tab")
-//let del = document.getElementById("del")
-let inp_el = document.getElementById("inp_el")
-let links = document.getElementById("links")
-let inp_arr=[]
-let dom=""
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
-inp_btn.addEventListener("click", save_inp);
-tab.addEventListener("click", save_tab);
-del.addEventListener("click", delete_inp);
-
-
-
-function save_inp(){
-    let input_value=
-    inp_arr.push(inp_el.value)
-    inp_el.value=""
-    render()
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
 
-function render(){
-    dom=""
-    for(let i=0; i<inp_arr.length; i++){
-        dom+=`<li>${inp_arr[i]}</li>`
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
     }
-    console.log(dom)
-    links.innerHTML=dom
+    ulEl.innerHTML = listItems
 }
 
-function save_tab(){
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        let url = tabs[0].url;
-        console.log(url)
-        // use `url` here inside the callback because it's asynchronous!
-    });
-    //inp_arr.push(url)
-    //render()
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
-}
-function delete_inp(){
-    console.log("delete all")
-}
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
